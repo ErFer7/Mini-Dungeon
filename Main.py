f5b3edec0d2bbd1627a29697469bded7fd2e6968
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-11-06 21:03:22 -0300

    Testes com gráficos

--- Main.py
+++ Main.py
@@ -1,16 +1,35 @@
+# V 0.0.3
+
+import sys
 import pygame
-import DungeonManager
+# import DungeonManager
 
-pygame.display.init()
+pygame.init()
+display = pygame.display.set_mode(flags = pygame.FULLSCREEN)
 
-DungeonManager.DungeonGenerator(100, 100)
+myfont = pygame.font.SysFont('Arial', 30)
+textsurface = myfont.render("MINI DUNGEON - V 0.0.3", False, (0, 0, 0))
+red = (255, 0, 0)
+rect = pygame.Rect(200, 150, 100, 50)
 
-if pygame.display.get_init():
+while True:
 
-    pygame.display.set_mode(flags = pygame.FULLSCREEN)
-    print("Display inicializado")
-else:
+    events = pygame.event.get()
 
-    print("Falha no display")
+    for event in events:
 
-pygame.display.quit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
+
+            print("Display finalizado")
+            pygame.quit()
+            sys.exit()
+    
+    mousePos = pygame.mouse.get_pos()
+
+    display.fill((255, 255, 255))
+    surf = pygame.Surface((100, 50))
+    pygame.draw.rect(surf, red, rect)
+    display.blit(surf, mousePos)
+    display.blit(textsurface,(0,0))
+    pygame.display.update()
+    pygame.time.delay(1)
